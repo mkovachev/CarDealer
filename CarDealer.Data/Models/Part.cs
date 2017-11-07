8a8edfc5df3b9588d00ae79329f1c4a8d311db76
mkovachev <martin.kovachev@outlook.com>
2017-11-07 11:02:31 +0200

    .

--- CarDealer.Data/Models/Part.cs
+++ CarDealer.Data/Models/Part.cs
@@ -13,8 +13,9 @@ namespace CarDealer.Data.Models
         [Required]
         public string Name { get; set; }
 
+        [Range(0, double.MaxValue)]
         [Column(TypeName = "money")]
-        public decimal? Price { get; set; }
+        public double? Price { get; set; }
 
         public int Quantity { get; set; }
 
