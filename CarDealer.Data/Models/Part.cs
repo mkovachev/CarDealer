936a5b21938bff08329cc6d4f93b4e193d8419e5
mkovachev <martin.kovachev@outlook.com>
2017-11-07 11:00:47 +0200

    Price changed to decimal

--- CarDealer.Data/Models/Part.cs
+++ CarDealer.Data/Models/Part.cs
@@ -16,7 +16,7 @@ namespace CarDealer.Data.Models
         public string Name { get; set; }
 
         [Column(TypeName = "money")]
-        public double? Price { get; set; }
+        public decimal? Price { get; set; }
 
         public int Quantity { get; set; }
 
